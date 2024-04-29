7377a88f9f8076154f7868a50a4276570d2ed817
Yehor Chernenko <yehorchernenko@lyft.com>
2024-04-29 18:40:26 +0300

    Fix build

--- ShadowBoxing/ImmersiveView/RealityKit/Extensions/Simulator+Extensions.swift
+++ ShadowBoxing/ImmersiveView/RealityKit/Extensions/Simulator+Extensions.swift
@@ -4,6 +4,8 @@ extension View {
     public func simulatorOnlyGesture<T>(_ gesture: T, including mask: GestureMask = .all) -> some View where T : Gesture {
         #if targetEnvironment(simulator)
             self.gesture(gesture, including: mask)
+        #else
+            self
         #endif
     }
 }
